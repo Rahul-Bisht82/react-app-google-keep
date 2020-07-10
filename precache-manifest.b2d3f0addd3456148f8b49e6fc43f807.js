e2bd9874de80e6094c87523524ebd7d7c72d967c
rahul bisht <rahul.b4749@gmail.com>
2020-07-10 20:27:23 +0530

    Updates

---
+++ precache-manifest.b2d3f0addd3456148f8b49e6fc43f807.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258442c9531796cc729e60231973a442",
+    "url": "/react-app-google-keep/index.html"
+  },
+  {
+    "revision": "c86a678f151899c4c807",
+    "url": "/react-app-google-keep/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f8866d28578960dd6c5f",
+    "url": "/react-app-google-keep/static/css/main.f94e64bb.chunk.css"
+  },
+  {
+    "revision": "c86a678f151899c4c807",
+    "url": "/react-app-google-keep/static/js/2.4f8f0342.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-app-google-keep/static/js/2.4f8f0342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8866d28578960dd6c5f",
+    "url": "/react-app-google-keep/static/js/main.42bcf126.chunk.js"
+  },
+  {
+    "revision": "1424a5ec82bdf01383cf",
+    "url": "/react-app-google-keep/static/js/runtime-main.fd1b7142.js"
+  }
+]);
